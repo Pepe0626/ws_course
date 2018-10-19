161b1b80995aae185b9999bc86d8aa549a0cc82a
jmeza2693 <jmeza@consistent.com.mx>
2018-10-19 13:55:57 -0500

    Se crea rama SOAP

---
+++ HelloWebService/src/main/java/com/consistent/ws/soap/config/WebServiceConfig.java
@@ -0,0 +1,28 @@
+package com.consistent.ws.soap.config;
+
+import javax.xml.ws.Endpoint;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import com.consistent.ws.soap.HelloWS;
+
+@Configuration
+public class WebServiceConfig {
+
+	@Autowired
+	private Bus bus;
+	
+	@Bean
+	public Endpoint endPoint() {
+		
+		EndpointImpl endpoint = new EndpointImpl(bus, new HelloWS());
+		endpoint.publish("/hello");
+		return endpoint;
+	}
+	
+}
